2e605b235d5dcf5ac830fccde07e5c4e3958d2b6
abbasio <omarabbasi16@gmail.com>
2021-07-31 12:27:18 -0400

    Fixed initial grid setup

--- sketch.js
+++ sketch.js
@@ -1,28 +1,40 @@
-//---------GRID SETUP
-
-const slider = document.querySelector("#slider");
-const output = document.querySelector("#dim");
-output.textContent = `${slider.value} x ${slider.value}`;
+//---------INITIAL GRID
 
 const grid = document.querySelector('#container');
-let gridSize = slider.value;
-grid.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+const slider = document.querySelector("#slider");
+const output = document.querySelector("#dim");
+slider.value = 16;
+output.textContent = `${slider.value} x ${slider.value}`;
 
+makeGrid();
+
+
+//---------UPDATE GRID
 
 slider.oninput = function () {
     output.textContent = `${slider.value} x ${slider.value}`;
+    grid.innerHTML="";
+    makeGrid();
 }
 
 
-//---------GRID SIZE
-for (let i = 0; i < (gridSize * gridSize); i++){
-    const addGrid = document.createElement('div');
-    addGrid.addEventListener('mouseover', drawColor);
-    addGrid.classList.add('cell');
-    grid.appendChild(addGrid);
+//---------MAKE GRID
+
+function makeGrid() {
+
+    gridSize = slider.value;
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
+
+    for (let i = 0; i < (gridSize * gridSize); i++){
+        const addGrid = document.createElement('div');
+        addGrid.addEventListener('mouseover', drawColor);
+        addGrid.classList.add('cell');
+        grid.appendChild(addGrid);
+    }
+
 }
 
-//---------GRID DRAW
+//---------DRAW ON GRID
 
 function drawColor(e){
     e.target.style.backgroundColor = ("black");
