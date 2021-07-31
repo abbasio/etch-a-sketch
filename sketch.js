7772da7136eba816f91dd5903bbbf44d46fcce30
abbasio <omarabbasi16@gmail.com>
2021-07-31 11:55:07 -0400

    Added slider for variable grid size

--- sketch.js
+++ sketch.js
@@ -1,17 +1,33 @@
+//---------GRID SETUP
+
+const slider = document.querySelector("#slider");
+const output = document.querySelector("#dim");
+output.textContent = `${slider.value} x ${slider.value}`;
+
 const grid = document.querySelector('#container');
-let gridSize = 256;
+let gridSize = slider.value;
+grid.style.gridTemplateColumns = `repeat(${gridSize}, auto)`;
 
 
-for (let i = 0; i < gridSize; i++){
+slider.oninput = function () {
+    output.textContent = `${slider.value} x ${slider.value}`;
+}
+
+
+//---------GRID SIZE
+for (let i = 0; i < (gridSize * gridSize); i++){
     const addGrid = document.createElement('div');
     addGrid.addEventListener('mouseover', drawColor);
     addGrid.classList.add('cell');
     grid.appendChild(addGrid);
 }
 
+//---------GRID DRAW
 
 function drawColor(e){
     e.target.style.backgroundColor = ("black");
 }
+
+
 
 
